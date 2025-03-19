2ef6492ff2770dd2048c7ca0a0c60e86047069e4
Shreyansh Verma <161208039+shreyanshv001@users.noreply.github.com>
2025-03-19 22:53:08 +0530

    added

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -140,6 +140,22 @@ router.get("/rejected-ngos", authAdminMiddleware, async (req, res) => {
     res.status(500).json({ message: "Error fetching rejected NGOs" });
   }
 });
+// Route to get NGO information by ID
+router.get("/:id", async (req, res) => {
+  console.log("Fetching NGO with ID:", req.params.id); // Debug log
+  try {
+    const ngo = await NGOModel.findById(req.params.id).select(
+      "-password -otp -otpExpires -registrationDate -__v"
+    );
+    if (!ngo) {
+      return res.status(404).json({ message: "NGO not found" });
+    }
+    res.status(200).json(ngo);
+  } catch (error) {
+    console.error("Error fetching NGO:", error);
+    res.status(500).json({ message: "Error fetching NGO" });
+  }
+});
 
 router.get("/:type-support", authAdminMiddleware, async (req, res) => {
   const { type } = req.params;
