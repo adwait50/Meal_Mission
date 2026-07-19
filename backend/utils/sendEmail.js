69781f0792e0bd06c76b09cf9ebab47553d30d23
Adwait <Adwaitkhare111@gmail.com>
2026-07-19 07:59:29 +0530

    email fix

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,19 +1,11 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
   try {
-    await transporter.sendMail({
-      from: `"Meal Mission" <${process.env.EMAIL_USER}>`,
+    await resend.emails.send({
+      from: "Meal Mission <onboarding@resend.dev>",
       to,
       subject,
       text,
